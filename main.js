f57aa976cef05a6f24b7eb422e0a4af1cd4282ca
ashLuchowa <ashwinluchowa@gmail.com>
2024-02-09 23:59:18 +0800

    primary table style created + new Book function

--- main.js
+++ main.js
@@ -10,6 +10,18 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
+//create a new book form
+function createNewBook() {
+    const newForm = document.createElement('div');
+    newForm.classList.add('main-table');
+    const mainContainer = document.querySelector('.main-container');
+    mainContainer.appendChild(newForm);
+    const tableTitle = document.createElement('div');
+    newForm.appendChild(tableTitle);
+    tableTitle.classList.add('mainTitle');
+    tableTitle.textContent = `Title: ${title.value}`;
+}
+
 //Submit button Event Listener
 function submitForm(x) {
     x.preventDefault();
@@ -19,6 +31,7 @@ function submitForm(x) {
     read = document.getElementById('read');
     const addedBook = new Book(title.value, author.value, pages.value, read.checked);
     myLibrary.push(addedBook);
+    createNewBook();
     console.log(addedBook);
 };
 
